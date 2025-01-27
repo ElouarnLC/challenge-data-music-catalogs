5479fcd98850ed51f125d2f75ca64ec5e877f9f8
ElouarnLC <61230623+ElouarnLC@users.noreply.github.com>
2025-01-27 18:34:41 +0100

    tags + categs + preds

---
+++ music-catalogs-classifier/music_catalogs_classifier/web-app/0_🏠_Home.py
@@ -0,0 +1,14 @@
+# App streamlit pour présenter notre projet et nos résultats obtenus avec les différents modèles
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.set_page_config()
+
+# Titre de la page
+st.title("Classification de catalogues de musique")
+
+st.divider()
---
+++ music-catalogs-classifier/music_catalogs_classifier/web-app/pages/2_📊_Résultats.py
@@ -0,0 +1,14 @@
+# App streamlit pour présenter notre projet et nos résultats obtenus avec les différents modèles
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Titre de la page
+st.title("Classification de catalogues de musique")
+
+st.divider()
+
+st.header("Démo des résultats obtenus avec les différents modèles")
